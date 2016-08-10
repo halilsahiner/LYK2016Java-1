428cb2e944e4a59d18e75676b39e6ff6bfd174ca
Ceyhun Onur <ceyonur@ku.edu.tr>
2016-08-10 10:59:08 +0300

    cardguessinggame eklendi

---
+++ src/cardguessinggame/CardGuessingGame.java
@@ -0,0 +1,39 @@
+package cardguessinggame;
+
+
+import java.util.ArrayList;
+
+//
+//In this question, your task is implementing a card guessing game. There are four card type
+//such as “spade”, “heart”, “diamond” and “club”. Each card can have value 1 to 13. The
+//program chooses a card from deck and asks player card type and value. The program informs
+//player about whether his/her guess is correct or wrong. Program runs until player guess is
+//correct.
+public class CardGuessingGame {
+	public static void main(String args[]){
+		//your code starts here
+		//generates a card deck and picks a random card from the deck.		
+		
+		System.out.println("Welcome to the card guessing game!\n");
+		
+		//Takes cardType and value inputs from the user and creates a card from inputs then checks if user has guessed the card correct or wrong.		
+		//Program asks new inputs from the user until the user guesses correct.
+		while(true){
+			//...
+	
+		}
+		//Your code ends here
+		
+	}
+	
+	/*
+	 * Generates card deck which is an arrayList that holds all cards with all types and values.
+	 * there must be 52 cards in deck with types "spade", "club", "diamond", "heart" and values between 1-13
+	 * */
+	public ArrayList<Card> generateCardDeck(){
+		
+		
+	}
+	
+
+}
